8dc09622924c834d4705703a8479eab698430d79
Miguel_p.silva <33907166+MiguelProgrammer@users.noreply.github.com>
2024-09-19 20:35:41 -0300

    :rocket: restTamplate to neighborfood access resource

--- app/src/main/java/br/com/food/api/access/Handler.java
+++ app/src/main/java/br/com/food/api/access/Handler.java
@@ -1,27 +1,29 @@
 package br.com.food.api.access;
 
+import br.com.food.api.access.adapter.in.ClienteRequest;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.springframework.web.client.RestTemplate;
 
-public class Handler implements
-        RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     @Override
-    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent request,
-                                                      Context context) {
+    public APIGatewayProxyResponseEvent handleRequest(
+            APIGatewayProxyRequestEvent request, Context context) {
+
         var logger = context.getLogger();
 
         logger.log("Request received - " + request.getBody());
+        RestTemplate restTemplate = new RestTemplate();
+        String fooResourceUrl = "http://localhost:80/neighborfood/login";
+
+        ClienteRequest clientResponse = restTemplate.getForEntity(fooResourceUrl, ClienteRequest.class).getBody();
 
         return new APIGatewayProxyResponseEvent()
                 .withStatusCode(200)
-                .withBody("""
-                        {
-                            "sucesso": true
-                        }
-                        """)
+                .withBody(clientResponse != null ? clientResponse.toString() : new ClienteRequest().toString())
                 .withIsBase64Encoded(false);
     }
 
---
+++ app/src/main/java/br/com/food/api/access/adapter/in/ClienteRequest.java
@@ -0,0 +1,51 @@
+package br.com.food.api.access.adapter.in;
+
+public class ClienteRequest {
+
+    private String nome;
+    private String email;
+    private String cpf;
+
+    public ClienteRequest() {
+    }
+
+    public ClienteRequest(String nome, String email, String cpf) {
+        this.nome = nome;
+        this.email = email;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "ClienteRequest{" +
+                "nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", cpf='" + cpf + '\'' +
+                '}';
+    }
+}
+
